aca6897ba242a689acf8fe63f1fcf9a4fadfbe64
Karl Svensson <analista64@gmail.com>
2019-03-22 19:07:07 +0100

    salvar, tabs

--- kgs.js
+++ kgs.js
@@ -1,17 +1,59 @@
+function getText() {
+  return document.getElementById('data').value;
+}
+
 function generate() {
     canvasReset();
-    addElement("canvas",'span','top','Top Bar');
+    var text = getText();
+    var tabs = getTabs(text);
+    createTabs(tabs);
+}
+
+function getTabs(text) {
+  var tabs = [];
+  var temp = text.split('FORM:');
+  temp.forEach(tab => {
+    if (tab) {
+      if (tab.indexOf('\n') > -1) {
+        tabs.push(tab.substr(0,tab.indexOf('\n')));
+      } else {
+        tabs.push(tab);
+      }
+    }
+  });
+  return tabs;
 }
 
 function canvasReset() {
-    var p = document.getElementById("canvas");
-    p.innerHTML = '';
+  var p = document.getElementById("canvas");
+  //p.innerHTML = '';  
 }
 
-function addElement(parentId, elementTag, elementId, html) {
-    var p = document.getElementById(parentId);
-    var newElement = document.createElement(elementTag);
-    newElement.setAttribute('id', elementId);
-    newElement.innerHTML = '<h3 class="alert alert-secondary">TOP BAR</h3>';
-    p.appendChild(newElement);
+function addElement(parentId, elementTag, where) {
+  if (!where) where = "beforeend";
+  var p = document.getElementById(parentId);
+  p.insertAdjacentHTML(where, elementTag);
+}
+
+function createTabs(tabs) {
+  // No saltamos el Home
+  for (let index = 1; index < tabs.length; index++) {
+    addElement("nav-tab", htmlNavTab('nav-tab-'+index, tabs[index]));
+    addElement("nav-tabContent", htmlTabContent('tab-content-'+index));
+  };
+}
+
+function htmlNavTab(id, title) {
+  var s ='<a class="nav-item nav-link" id="'+ id +'" data-toggle="tab" href="#'+ id +'">'+ title +'</a>';
+  return s;
+}
+function htmlTabContent(id) {
+  return '<div class="tab-pane fade show" id="'+id+'">'+id+'Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.</div>';
+}
+
+function guardar() {
+  var filename = document.getElementById('project').value;
+  var text = getText();
+  var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
+  saveAs(blob, filename+".kgs");
 }
