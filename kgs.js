5b9275c89664acdfc3fced943fa09b32f705ebbe
Karl Svensson <analista64@gmail.com>
2019-03-23 13:44:43 +0100

    horas

--- kgs.js
+++ kgs.js
@@ -38,17 +38,37 @@ function addElement(parentId, elementTag, where) {
 function createTabs(tabs) {
   // No saltamos el Home
   for (let index = 1; index < tabs.length; index++) {
-    addElement("nav-tab", htmlNavTab('nav-tab-'+index, tabs[index]));
+    addElement("nav-tab", htmlNavTab('nav-tab-'+index, tabs[index], 'tab-content-'+index));
     addElement("nav-tabContent", htmlTabContent('tab-content-'+index));
   };
 }
 
-function htmlNavTab(id, title) {
-  var s ='<a class="nav-item nav-link" id="'+ id +'" data-toggle="tab" href="#'+ id +'">'+ title +'</a>';
+function htmlNavTab(id, title, contentId) {
+  var s ='<a class="nav-item nav-link" id="'+ id +'" data-toggle="tab" href="#'+ contentId +'">'+ title +'</a>';
   return s;
 }
 function htmlTabContent(id) {
-  return '<div class="tab-pane fade show" id="'+id+'">'+id+'Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.</div>';
+  var html = '<div class="tab-pane fade show" id="'+id+'">'+
+    '  <h5 class="alert alert-primary">Horas</h5>'+
+    '  <input type="button" class="btn btn-block btn-danger" value="Volver a Home"/>'+
+    '  <div class="row" style="height:400px;">'+
+    '    <div class="card col-sm-12 d-table h-100">'+
+    '      <div class="d-table-cell align-middle">'+
+    '       <center>'+
+    '        <ul class="list-group" id="horas">'+
+    '          <li class="list-group-item">13:05:50</li>'+
+    '          <li class="list-group-item">13:05:44</li>'+
+    '          <li class="list-group-item">13:05:25</li>'+
+    '          <li class="list-group-item">13:05:22</li>'+
+    '          <li class="list-group-item">13:05:10</li>'+
+    '        </ul>'+
+    '       </center>'+
+    '      </div>'+
+    '    </div>'+
+    '  </div>'+
+    '  </div>'+
+    '</div>';
+  return html;
 }
 
 function guardar() {
